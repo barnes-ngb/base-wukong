b56d7dbe550dd679ecf7afa48d04f6a92f438d16
Nathan Barnes <barnes.ngb@gmail.com>
2026-09-06 17:37:36 -0500

    wukong install

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+angle = 135
+STEP = 30
+LO = 15
+HI = 165
+
+def move_to(a):
+    wuKong.set_servo_angle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, a)
+        basic.show_number(a)
+
+        def on_button_a():
+            global angle
+                angle = min(HI, angle + STEP)
+                    move_to(angle)
+                    input.on_button_pressed(Button.A, on_button_a)
+
+                    def on_button_b():
+                        global angle
+                            angle = max(LO, angle - STEP)
+                                move_to(angle)
+                                input.on_button_pressed(Button.B, on_button_b)
+
+                                move_to(angle)
